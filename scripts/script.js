3a82a1eb02f85d049421d080e203e34c420af7ef
Sheila Gonzalez <capacitaciones06.05@adviters.com>
2024-07-02 15:32:03 -0300

    arreglos

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     function* myGen(){
+        while(true){
         yield "Hay imagen 1";
         yield "Hay imagen 2";
         yield "Hay imagen 3";
         yield "Hay imagen 4";
+        }
     }
     
     const gen = myGen();
+
+    function reset(){
+        let objetoGen = gen.next()
+    }
 
     function nextImage(){
         objetoGen = gen.next()
         if (!objetoGen.done) {
             console.log("Hay imagen", objetoGen.value);
         } else {
-            console.log("no hay imagen");
+            console.log("no hay imagen");   
         }
         
     }
